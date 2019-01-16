4ab4769a309527845fb5a3f84f3d58c99095ee50
Kyruski <jamescboyett@gmail.com>
2019-01-16 12:31:19 -0600

    Implements working Dynamic GroceryList

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 var GroceryList = () => (
+  <GroceryListItem item={['Banana', 'Apple', 'Carrot']} />
+);
+
+var GroceryListItem = (props) => (
   <ul>
-    <li>Banana</li>
-    <li>Apple</li>
+    <li>{props.item[0]}</li>
+    <li>{props.item[1]}</li>
+    <li>{props.item[2]}</li>
   </ul>
 );
+
+// var Banana = () => (
+//   <li>Banana</li>
+// );
+
+// var Apple = () => (
+//   <li>Apple</li>
+// );
 
 // TODO
 ReactDOM.render(<GroceryList />, document.getElementById('app'));
