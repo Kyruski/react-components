7facf9de865d95074a444a8383d11c7f3809f220
Kyruski <jamescboyett@gmail.com>
2019-01-16 15:29:12 -0600

    Implements Classes for React

--- app.js
+++ app.js
@@ -1,22 +1,50 @@
-var GroceryList = () => (
-  <GroceryListItem item={['Banana', 'Apple', 'Carrot']} />
-);
 
-var GroceryListItem = (props) => (
+
+class GroceryListItem extends React.Component { 
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    }
+  }
+    
+  onListItemClick() {
+    this.setState ({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+
+    return (
+     <li style={style} onMouseOver={this.onListItemClick.bind(this)}>{this.props.item[0]}</li>
+    );
+ } 
+};
+
+var GroceryList = (props) => (
   <ul>
-    <li>{props.item[0]}</li>
-    <li>{props.item[1]}</li>
-    <li>{props.item[2]}</li>
+    {props.items.map((item, index) => <GroceryListItem item={[item, index]}/>)}
   </ul>
 );
 
+var App = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList items ={['banana', 'apple']}/>
+  </div>
+)
+
 // var Banana = () => (
-//   <li>Banana</li>
-// );
-
-// var Apple = () => (
-//   <li>Apple</li>
-// );
-
-// TODO
-ReactDOM.render(<GroceryList />, document.getElementById('app'));
+  //   <li>Banana</li>
+  // );
+  
+  // var Apple = () => (
+    //   <li>Apple</li>
+    // );
+    
+    // TODO
+ReactDOM.render(<App />, document.getElementById('app'));
